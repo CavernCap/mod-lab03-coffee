b15a1d1c18fa5657524f9add973b2c75a46b0f71
CavernCap <148848111+CavernCap@users.noreply.github.com>
2024-03-31 15:35:33 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+enum class STATES {
+    OFF,
+    ON,
+    WAITING,
+    ACCEPT,
+    COOKING
+};
+class Automata
+{
+private:
+    int cash;
+    std::vector<std::string> menu;
+    std::vector<int> prices;
+    STATES state;
+public:
+    Automata();
+    void on();
+    void off();
+    void coin(int amount);
+    void getMenu();
+    STATES getState() const;
+    int getCash() const;
+    void choice(const std::string& item);
+    bool check(int price) const;
+    void cancel();
+    void cook(const std::string& item);
+    void finish();
+};
