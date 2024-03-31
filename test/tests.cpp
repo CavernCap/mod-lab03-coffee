53005be1f5895207227b37622a145d47be9372cb
CavernCap <148848111+CavernCap@users.noreply.github.com>
2024-03-31 15:36:50 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,49 @@
-// Copyright 2022 GHA Test Team
-
-#include <gtest/gtest.h>
+#include "pch.h"
 #include "Automata.h"
+
+TEST(automata, turn_it_on) {
+	Automata automata;
+	automata.on();
+	STATES current_state = automata.getState();
+  EXPECT_EQ(STATES::ON,current_state);
+}
+
+TEST(automata, turn_it_off) {
+	Automata automata;
+	automata.on();
+	automata.off();
+	STATES current_state = automata.getState();
+	EXPECT_EQ(STATES::OFF, current_state);
+}
+
+TEST(automata, coin_balance) {
+	Automata automata;
+	automata.on();
+	automata.coin(10);
+	STATES current_state = automata.getState();
+	int current_balance = automata.getCash();
+	EXPECT_EQ(STATES::WAITING, current_state);
+	EXPECT_EQ(10, current_balance);
+}
+
+TEST(automata, buy_drink) {
+	Automata automata;
+	automata.on();
+	automata.coin(60);
+	automata.choice("HotChocolate");
+	STATES current_state = automata.getState();
+	int current_balance = automata.getCash();
+	EXPECT_EQ(STATES::ON, current_state);
+	EXPECT_EQ(0, current_balance);
+}
+
+TEST(automata, cancel_operation) {
+	Automata automata;
+	automata.on();
+	automata.coin(100);
+	automata.cancel();
+	STATES current_state = automata.getState();
+	int current_balance = automata.getCash();
+	EXPECT_EQ(STATES::ON, current_state);
+	EXPECT_EQ(0, current_balance);
+}
